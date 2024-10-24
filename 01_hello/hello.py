fe7608fef3b547e7e3d69e3bb6979748871370df
Oliver <oliver.gross.og@gmx.de>
2024-10-24 22:19:06 +0200

    creating get_args function and main function

---
+++ 01_hello/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# say hello
+
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument('-n', '--name', metavar="name", default="World", help='Name to greet')
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    name = args.name
+    print ("Hello, " + name + "!")
+
+
+if __name__ == '__main__':
+    main()
